13e0c18b750cd550f781e9eb78ec7da5ae9221b7
tedekeroth <ted.ekeroth@gmail.com>
2021-08-02 11:18:51 +0200

    Add files via upload

---
+++ TestAppHost.cs
@@ -0,0 +1,35 @@
+﻿using Funq;
+using Microsoft.AspNetCore.Http;
+using ServiceStack;
+using ServiceStack.Api.OpenApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace TestSS
+{
+    public class TestAppHost : AppHostBase
+    {
+        public ILogger Logger { get; set; }
+
+        public TestAppHost(string serviceName, params Assembly[] assembliesWithServices) : base(serviceName, assembliesWithServices)
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new OpenApiFeature());
+            container.AddSingleton<ILogger, TestLogger>();
+            container.AutoWire(this);
+        }
+
+        public override Task ProcessRequest(HttpContext context, Func<Task> next)
+        {
+            Logger?.Log("hmm");
+
+            return base.ProcessRequest(context, next);
+        }
+    }
+}
---
+++ TestLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestSS
+{
+    public class TestLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
